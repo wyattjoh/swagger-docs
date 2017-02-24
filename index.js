21be1ae966f4e3f1a5ba1f6e825cafbbc5bf96b9
Wyatt Johnson <wyattjoh@gmail.com>
2017-02-23 18:52:28 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const redis = require('redis');
+const client = redis.createClient(process.env.REDIS_URL);
+const GitHubStrategy = require('passport-github').Strategy;
+const github = require('octonode');
+const querystring = require('querystring');
+
+const app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: `${process.env.ROOT_URL}/auth/github/callback`,
+    scope: ['repo']
+  }, (accessToken, refreshToken, profile, done) => {
+    done(null, {accessToken, profile});
+  }
+));
+
+passport.serializeUser((user, done) => { done(null, user); });
+
+passport.deserializeUser((user, done) => { done(null, user); });
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: true,
+  store: new RedisStore({client}),
+  cookie: {}
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/logout', (req, res) => {
+  delete req.session.passport;
+  res.redirect('/');
+});
+app.get('/auth/github', passport.authenticate('github'));
+app.get('/auth/github/callback', passport.authenticate('github', {
+  failureRedirect: '/login'
+}), (req, res) => {
+  res.redirect('/');
+});
+
+app.use((req, res, next) => {
+  if (req.user) {
+    req.client = github.client(req.user.accessToken);
+  }
+
+  next();
+})
+
+app.get('/', (req, res) => {
+  let ctx = {user: req.user};
+
+  if (ctx.user) {
+    req.client.me().repos({
+      per_page: 100
+    }, (err, data) => {
+      if (err) {
+        return next(err);
+      }
+
+      ctx.repos = data;
+
+      res.render('pages/index', ctx);
+    });
+  } else {
+    res.render('pages/index', ctx);
+  }
+});
+
+app.get('/swagger', (req, res, next) => {
+  req.client
+    .repo(req.query.repo)
+    .contents(req.query.filename, (err, data) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.redirect(302, data.download_url);
+    });
+});
+
+app.get('/docs', (req, res) => {
+  res.render('pages/docs', {
+    doc: '/swagger?' + querystring.stringify({
+      repo: req.query.repo,
+      filename: req.query.filename
+    }),
+    host: req.query.host ? (req.query.host.length > 0 ? req.query.host : null) : null
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
